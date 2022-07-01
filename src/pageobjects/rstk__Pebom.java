5be0d2ac587664a2f6910da5d6b6e54f0dba1c7b
Mayur Suryawanshi <msuryawanshi@rootstock.com>
2022-07-01 21:44:48 +0530

    Changes

--- src/pageobjects/rstk__Pebom.java
+++ src/pageobjects/rstk__Pebom.java
@@ -18,52 +18,50 @@ import com.provar.core.testapi.annotations.SalesforcePage;
 import com.provar.core.testapi.annotations.TestLogger;
 import com.provar.core.testapi.annotations.TextType;
 
-@SalesforcePage( title="Rstk__ Pebom"                                
-               , summary=""
-               , page="Pebom"
-               , namespacePrefix="rstk"
-               , object="rstk__pebom__c"
-               , connection="QARSF_Admin"
-     )             
+@SalesforcePage(title = "Rstk__ Pebom", summary = "", page = "Pebom", namespacePrefix = "rstk", object = "rstk__pebom__c", connection = "QARSF_Admin")
 public class rstk__Pebom {
 
-	
 	public WebDriver driver;
 
 	public rstk__Pebom(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+
 	@TextType()
 	@FindBy(xpath = "//input[@id='pebom_item__c_autocomplete']")
 	public WebElement parentItem;
+
 	@TextType()
 	@FindBy(xpath = "//li[@id='li-0']")
 	public WebElement list;
+
 	@TextType()
 	@FindBy(xpath = "//input[@id='pebom_compitem__c_autocomplete']")
 	public WebElement componentItem;
+
 	@TextType()
 	@FindBy(xpath = "//label[normalize-space(.)='Quantity Per']/parent::span/parent::th/following-sibling::td//input")
 	public WebElement quantityPer;
+
 	@TextType()
 	@FindBy(xpath = "//td[contains(@id,'tab_Mrpoverrides_lbl')]")
 	public WebElement tab_Mrpoverrides_lbl;
+
 	@TextType()
 	@FindBy(xpath = "//label[normalize-space(.)='Scrap Factor']/parent::span/parent::th/following-sibling::td[1]//input")
 	public WebElement scrapFactor;
+
 	@TextType()
 	@FindBy(xpath = "//label[normalize-space(.)='Setup Quantity']/parent::span/parent::th/following-sibling::td[1]//input")
 	public WebElement setupQuantity;
+
 	@ButtonType()
 	@FindByLabel(label = "Save")
 	public WebElement save;
 
-	
 	@TestLogger
 	public Logger testLogger;
-	
-	
+
 	public void selectParentItem(String parentItemName) throws InterruptedException {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 
@@ -71,7 +69,7 @@ public class rstk__Pebom {
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
 
 		WebElement ele = driver.findElement(By.xpath(xpath));
-
+		ele.clear();
 		ele.sendKeys(parentItemName);
 		Thread.sleep(3000);
 		Actions actions = new Actions(driver);
@@ -102,46 +100,5 @@ public class rstk__Pebom {
 		}
 
 	}
-	public	void selectComponentItem(String componentItemName) throws InterruptedException {
-		WebDriverWait wait = new WebDriverWait(driver, 30);
 
-		String xpath = "//input[@id='pebom_compitem__c_autocomplete']";
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
-
-		WebElement ele = driver.findElement(By.xpath(xpath));
-
-		ele.sendKeys(componentItemName);
-		Thread.sleep(3000);
-		Actions actions = new Actions(driver);
-		wait.until(
-				ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='ac_results'][2]/ul[@id='IDREF']/li")));
-
-		List<WebElement> autoCompleteList = driver
-				.findElements(By.xpath("//div[@class='ac_results'][2]/ul[@id='IDREF']/li"));
-
-		testLogger.info("Size" + autoCompleteList.size());
-
-		while(autoCompleteList.size() > 10 || autoCompleteList.isEmpty()) {
-			ele.sendKeys(Keys.BACK_SPACE);
-			Thread.sleep(3000);
-			autoCompleteList = driver.findElements(By.xpath("//div[@class='ac_results'][2]/ul[@id='IDREF']/li"));
-
-		}
-
-		for (int i = 0; i < autoCompleteList.size(); i++) {
-			Thread.sleep(500);
-			actions.moveToElement(autoCompleteList.get(i)).build().perform();
-			testLogger.info("Parent Item Name" + autoCompleteList.get(i).getText());
-
-			if (autoCompleteList.get(i).getText().startsWith(componentItemName)) {
-				actions.moveToElement(autoCompleteList.get(i)).click().build().perform();
-				break;
-			}
-		}
-
-	}	
-	
-	
-	
-	
 }
